5f2a547e8ce5fbb744c32f3a7f08e7ac16a39b76
im-alexis <alexistorres@utexas.edu>
2022-08-31 14:50:34 -0500

    Finished Problem 1

--- src/Problem1.java
+++ src/Problem1.java
@@ -1,7 +1,20 @@
 public class Problem1 {
-    static public int solution (String input, int adjacent){
-       int total,highest = 0;
-
+    static public int problem1Solution (String input, int adjacent){
+       int total ,highest ,num;
+        total = highest = 1;
+    for(int i = 0; i < input.length(); i++){
+        if((input.length() - i) - 1 < adjacent){
+            break;
+        }
+        for(int j = 0; j < adjacent; j++){
+            num = input.charAt(j + i) - '0';
+            total = total * num;
+        }
+        if (total > highest){
+            highest = total;
+        }
+        total = 1;
+    }
         return highest;
     }
 }
