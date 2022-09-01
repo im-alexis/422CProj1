626063f81fc09ada66f898f235bbabca0b05785e
im-alexis <alexistorres@utexas.edu>
2022-09-01 00:42:58 -0500

    Finished Problem 3, Added the tagger jar file to project as well as Prop file

--- src/Problem2.java
+++ src/Problem2.java
@@ -1,20 +1,25 @@
+/* Student Name: Alexis Torres (at39625),
+ Lab Section:(17629)
+ */
+
 public class Problem2 {
-    static void problem2Solution(String input){
+    static public void problem2Solution(String input){
         int runningSum = 0;
-        int startIndex = 0;
+        int startIndex = 0; // keep treck of the index of the first letter for substring
         for(int i = 0; i < input.length(); i++){
-            if((input.charAt(i) >= 0x41) && (input.charAt(i) <= 0x5A)){
-                runningSum = runningSum + (input.charAt(i) - 0x40);
+            if((input.charAt(i) >= 0x41) && (input.charAt(i) <= 0x5A)){ // for captial letters A - Z (hex values)
+                runningSum = runningSum + (input.charAt(i) - 0x40); // do -40 so the value associated with A is 1
             }
-           else if((input.charAt(i) >= 0x61) && (input.charAt(i) <= 0x7A)){
+           else if((input.charAt(i) >= 0x61) && (input.charAt(i) <= 0x7A)){ // lowercaser a - z (hex values)
                 runningSum = runningSum + (input.charAt(i) - 0x60);
             }
-           else{
+           else{ // everything else is a zero
                runningSum = runningSum + 0;
             }
-            if(input.charAt(i) == ' '){
+            if(input.charAt(i) == ' '){// if a there is a space then it is a new word
                 if(runningSum >= 100){
-                    System.out.println(input.substring(startIndex,i+1));
+                    System.out.println(input.substring(startIndex,i+1)); // since index i is a space then adding one would
+                                                                        // give the length of the word
 
 
                 }
--- src/Problem3.java
+++ src/Problem3.java
@@ -1,2 +1,14 @@
+/* Student Name: Alexis Torres (at39625),
+ Lab Section:(17629)
+ */
+
+import edu.stanford.nlp.tagger.maxent.MaxentTagger; // this is the library of the tagger
+
 public class Problem3 {
+    static public void problem3Solution (String input){
+        //create a new tagger object using props file I have
+      MaxentTagger tagger =  new MaxentTagger("/Users/alexistorres/Documents/Code/422C/Assignment1/tagger/english-left3words-distsim.tagger");
+      String tagged = tagger.tagString(input); // this function tags the string
+      System.out.println(tagged);
+    }
 }
